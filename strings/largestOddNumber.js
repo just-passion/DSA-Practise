a563473105ddcd690dd2075f047b29d216374a9c
just-passion <tinycolorfulhummingbird@gmail.com>
2026-02-08 22:59:49 +0530

    largest odd number

---
+++ strings/largestOddNumber.js
@@ -0,0 +1,28 @@
+// The task is to find the largest-valued odd number that can be formed from a given numeric string by removing trailing even digits. The number must be a substring starting from index 0 to some valid index.
+
+// Steps
+// Start from the end of the string and move backward.
+// Check if the current digit is odd using modulus % 2.
+// If an odd digit is found, return the substring from index 0 to that digit (inclusive).
+// If no odd digit exists in the string, return an empty string.
+// Dry Run
+// Input: "52"
+
+// Start from the end: ‘2’ → even → skip
+// Next: ‘5’ → odd → return "5"
+
+
+
+
+
+
+var largestOddNumber = function(s) {
+      let n = s.length - 1;
+      while (n >= 0) {
+          if (Number(s[n]) % 2 == 1) {
+              return s.substring(0, n + 1);
+          }
+          --n;
+      }
+      return "";
+  };
