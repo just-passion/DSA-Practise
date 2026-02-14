a38556704ee23420e8b70a401d76c65537ce0b36
just-passion <tinycolorfulhummingbird@gmail.com>
2026-02-14 16:47:39 +0530

    correction

--- arrays/bestTimeToBuyAndSellStocks.js
+++ arrays/bestTimeToBuyAndSellStocks.js
@@ -2,7 +2,7 @@ var maxProfit = function(prices){
     let maxProfit = 0;
     for(let i=0; i<prices.length; i++){
         for(let j=i+1; j<prices.length; j++){
-            if((prices[j] = prices[i]) > maxProfit){
+            if((prices[j] - prices[i]) > maxProfit){
                 maxProfit = prices[j] - prices[i];
             }
         }
